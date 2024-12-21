2c34ee951672918ff25c7319840ecdc320ee9c2e
Allan Sechrist <sechristallan@gmail.com>
2024-12-21 12:54:32 +0900

    add update to task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,14 +12,28 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = Task.new(task_params_new)
     @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params_edit)
     redirect_to task_path(@task)
   end
 
   private
 
-  def task_params
+  def task_params_new
     params.require(:task).permit(:name, :details)
+  end
+
+  def task_params_edit
+    params.require(:task).permit(:name, :details, :completed)
   end
 end
